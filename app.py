d2440ce06240d8f32a85a8795cb06fa5750f3534
Eric Kharitonashvili <ericsrealemail@gmail.com>
2023-10-06 01:50:08 -0400

    REAL REQS

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
+#from flask_cors import CORS
 from main import generate, transcribe
 import os
 # Create a Flask application
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/")
 def home():
